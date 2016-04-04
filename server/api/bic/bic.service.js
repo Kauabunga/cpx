929d5e63a7980dee67c1687023aa6c816a5d4952
Carson Bruce <carson.bruce@solnet.co.nz>
2016-04-05 10:47:56 +1200

    Added levies parser

--- server/api/bic/bic.service.js
+++ server/api/bic/bic.service.js
@@ -3,6 +3,7 @@
 
 
 import _ from 'lodash';
+import {PdfReader, Rule} from 'pdfreader';
 import request from 'request-promise';
 var jsonfile = require('bluebird').promisifyAll(require('jsonfile'));
 
@@ -12,12 +13,86 @@ const DIVISIONS_URL = `https://api.businessdescription.co.nz/api/industries/{{in
 const CLASSES_URL = `https://api.businessdescription.co.nz/api/divisions/{{divisionId}}/classes`;
 const BICS_URL = `https://api.businessdescription.co.nz/api/bics?filter[include]=cu&filter[include]=anzsic&filter[include]=bicrefs&filter[include]=historyBic`;
 const BICS_SEED_FILENAME =`${__dirname}/../../config/seed/bic.json`;
+const ACC_LEVIES_PDF_2015_2016 = `${__dirname}/../../config/seed/acc_levies.pdf`;
+const ACC_LEVIES_JSON_2015_2016 = `${__dirname}/../../config/seed/acc_levies.json`;
 
+
+function parseLevies(){
+  return jsonfile.readFileAsync(ACC_LEVIES_JSON_2015_2016)
+    .catch(err => {return {};})
+    .then(levies => {
+
+      if(_(levies).map().value().length > 0){ return levies; }
+
+      return new Promise((resolve, reject) => {
+
+        let cuToLeviesMap = {};
+
+        let currentCu = '';
+        let currentLevy = {};
+
+        let currentIndex = -1;
+        let currentIndexPropertyMap = {
+          1: 'desc',
+          2: 'lrg',
+          3: 'empOrSep',
+          4: 'cpxStd',
+          5: 'cpxLlwc',
+          6: 'rclWs'
+        };
+
+        new PdfReader().parseFileItems(ACC_LEVIES_PDF_2015_2016, function(err, item){
+
+          if(err){ return reject(err); }
+          if(! item ) { return resolve(cuToLeviesMap); }
+          if(! item.text ){ return; }
+
+          //PDF ITEM:  01110
+          //PDF ITEM:  Nursery production
+          //PDF ITEM:  010
+          //PDF ITEM:  $0.93
+          //PDF ITEM:  $1.24
+          //PDF ITEM:  $1.17
+          //PDF ITEM:  $0.46
+
+          try {
+            if (isCuText(item.text)) {
+              currentCu = item.text;
+              currentLevy = {};
+              cuToLeviesMap[currentCu] = currentLevy;
+              currentIndex = 1;
+            }
+            else if (currentIndex > 0 && currentIndex <= 6) {
+              currentLevy[currentIndexPropertyMap[currentIndex++]] = item.text;
+            }
+          }
+          catch(err){ reject(err); }
+        });
+
+      })
+        .then(levies => {
+          jsonfile.writeFileAsync(ACC_LEVIES_JSON_2015_2016, levies);
+          return levies;
+        });
+
+    });
+}
+
+
+
+export function search(query){
+  return getIndex()
+    .then(index => {
+      return index.query(query);
+    });
+}
+
+function getIndex(){}
+function generateIndex(){}
 
 export function index(){
   return Promise.resolve()
     .then(() => {
-
       //TODO should seed properly and use database
       return jsonfile.readFileAsync(BICS_SEED_FILENAME)
       .catch(err => {return {};})
@@ -55,7 +130,7 @@ export function index(){
                     .then(classes => {
                       return bicsRequest.then(bics => {
 
-                        console.log('Fetched /api/bics length', bics.length);
+                        console.log('Fetched external /api/bics length', bics.length);
 
                         let classesFormatted = _(classes).flatten().map(clazz => {
                           clazz.className = clazz.name;
@@ -128,3 +203,12 @@ function getClassesUrl(divisionId){
   if(!divisionId){ throw new Error('No division id passed')}
   return CLASSES_URL.replace('{{divisionId}}', divisionId);
 }
+
+function isCuText(text = ''){
+  return text.length === 5 && getCuRegex().test(text);
+}
+
+function getCuRegex(){
+  return /\b\d{5}\b/g;
+}
+
