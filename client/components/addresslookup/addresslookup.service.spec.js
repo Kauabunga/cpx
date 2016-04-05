653f5adbf253bb0ddc2a74f99776d8fe1bcdb8c2
Troy Shaw <troyshw@gmail.com>
2016-04-05 12:53:02 +1200

    Added test for address lookup.

--- client/components/addresslookup/addresslookup.service.spec.js
+++ client/components/addresslookup/addresslookup.service.spec.js
@@ -2,23 +2,28 @@
 
 describe('Service: addresslookup', function () {
 
-  var $service;
   var addresslookup;
 
   // load the service's module
   beforeEach(module('cpxApp'));
 
-  beforeEach(module(function ($provide) {
-
-    $provide.value("$window", {});
+  beforeEach(module(function ($provide, $windowProvider) {
+    $provide.value("$window", $windowProvider.$get());
   }));
 
   beforeEach(inject(function (_addresslookup_) {
     addresslookup = _addresslookup_;
   }));
 
-  it('placeholder test', function () {
+  it('inserts a dynamic google loading script', function () {
+    var googleScript = getLastScript();
 
+    (googleScript.src === 'https://maps.googleapis.com/maps/api/js?libraries=places&key=AIzaSyCatE8RgWKWgGhnp49Q7l9KtbPzXeAoc94').should.be.true;
   });
 
+  // Gets the last script loaded, which presumably should be the google script
+  function getLastScript() {
+    console.log(document.getElementsByTagName('script').length);
+    return document.getElementsByTagName('script')[document.getElementsByTagName('script').length - 1];
+  }
 });
