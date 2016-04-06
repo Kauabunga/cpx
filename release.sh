ee7d7719902a69944a893af9f66a34023c6f0fa3
Carson Bruce <carson.bruce@solnet.co.nz>
2016-04-06 15:11:40 +1200

    commented out release step

--- release.sh
+++ release.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-set -e # exit with nonzero exit code if anything fails
+#set -e # exit with nonzero exit code if anything fails
 
 
-grunt build && grunt buildcontrol:local
+#grunt build && grunt buildcontrol:local
 
-if [ "$GH_TOKEN" = "" ]
-then
-   git push https://github.com/Kauabunga/cpx.git build
-else
-   git push --force --quiet "https://${GH_TOKEN}@github.com/Kauabunga/cpx.git" build > /dev/null
-fi
+#if [ "$GH_TOKEN" = "" ]
+#then
+#   git push https://github.com/Kauabunga/cpx.git build
+#else
+#   git push --force --quiet "https://${GH_TOKEN}@github.com/Kauabunga/cpx.git" build > /dev/null
+#fi
