c29a8f4e616672f021123384c884c3e911d51b3b
Carson Bruce <carson.bruce@solnet.co.nz>
2016-04-13 11:28:49 +1200

    progress

--- client/components/cpx/cpx.service.details.js
+++ client/components/cpx/cpx.service.details.js
@@ -11,68 +11,93 @@ angular.module('cpxApp')
 
         {
           type: 'group',
-          hideExpression: 'model.unknownAccNumber',
           templateOptions: {
             fields: [
               {
-                type: 'label',
+                type: 'group',
+                hideExpression: 'model.unknownAccNumber',
                 templateOptions: {
-                  label: `What's your ACC number?`
+                  fields: [
+                    {
+                      type: 'label',
+                      templateOptions: {
+                        label: `What's your ACC number?`
+                      }
+                    },
+                    {
+                      key: 'accNumber',
+                      type: 'input',
+                      templateOptions: {
+                        pattern: /^([a-zA-Z][a-zA-Z]\d{7}|[a-zA-Z]\d{8})$/,
+                        patternValidationMessage: `That ACC number doesn't look quite right`
+                      }
+                    }
+                  ]
                 }
               },
               {
-                key: 'accNumber',
-                type: 'input',
+                type: 'group',
+                hideExpression: 'model.accNumber.length === 9',
                 templateOptions: {
-                  pattern: /^([a-zA-Z][a-zA-Z]\d{7}|[a-zA-Z]\d{8})$/,
-                  patternValidationMessage: `That ACC number doesn't look quite right`
+                  fields: [
+                    {
+                      type: 'button',
+                      hideExpression: 'model.unknownAccNumber',
+                      templateOptions: {
+                        label: `I don't know my ACC number`,
+                        click: function($event, model, form){
+                          return model.unknownAccNumber = true;
+                        }
+                      }
+                    },
+                    {
+                      type: 'group',
+                      hideExpression: '! model.unknownAccNumber && ! model.accNumber',
+                      templateOptions: {
+                        fields: [
+
+                          {
+                            type: 'label',
+                            templateOptions: {
+                              label: `What's your IRD number?`
+                            }
+                          },
+                          {
+                            key: 'irdNumber',
+                            type: 'input',
+                            templateOptions: {
+                              pattern: /^(\d{8}|\d{9})$/,
+                              patternValidationMessage: `That IRD number doesn't look quite right`
+                            }
+                          }
+                        ]
+                      }
+                    },
+                    {
+                      type: 'button',
+                      hideExpression: '! model.unknownAccNumber || ( model.irdNumber.length === 8 || model.irdNumber.length === 9 )',
+                      templateOptions: {
+                        label: `I've found my ACC number`,
+                        click: function($event, model, form){
+                          return model.unknownAccNumber = false;
+                        }
+                      }
+                    },
+                  ]
                 }
               }
             ]
           }
         },
 
-        {
-          type: 'button',
-          hideExpression: 'model.unknownAccNumber || model.accNumber.length === 9',
-          templateOptions: {
-            label: `I don't know my ACC number`,
-            click: function($event, model, form){
-              return model.unknownAccNumber = true;
-            }
-          }
-        },
 
-        {
-          type: 'button',
-          hideExpression: '! model.unknownAccNumber',
-          templateOptions: {
-            label: `I've found my ACC number`,
-            click: function($event, model, form){
-              return model.unknownAccNumber = false;
-            }
-          }
-        },
+
+
         {
           type: 'group',
-          hideExpression: '! model.unknownAccNumber && ! model.accNumber',
+          hideExpression: '((model.irdNumber.length !== 8 && model.irdNumber.length !== 9) && (model.accNumber.length !== 9))',
           templateOptions: {
             fields: [
-
-              {
-                type: 'label',
-                templateOptions: {
-                  label: `What's your IRD number?`
-                }
-              },
-              {
-                key: 'irdNumber',
-                type: 'input',
-                templateOptions: {
-                  pattern: /^(\d{8}|\d{9})$/,
-                  patternValidationMessage: `That IRD number doesn't look quite right`
-                }
-              },
               {
                 type: 'label',
                 templateOptions: {
